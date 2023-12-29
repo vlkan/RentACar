b67775b6c53654112e7ae03a802bfc452f4b4044
vlkan <thkvolkanonder@gmail.com>
2023-12-29 03:01:01 +0300

    ..

--- Persistence/EntityConfigurations/BrandConfiguration.cs
+++ Persistence/EntityConfigurations/BrandConfiguration.cs
@@ -8,8 +8,14 @@ public class BrandConfiguration : IEntityTypeConfiguration<Brand>
 {
     public void Configure(EntityTypeBuilder<Brand> builder)
     {
-        builder.ToTable("Brands");
+        builder.ToTable("Brands").HasKey(b => b.Id);
 
-        builder.Property(b => b.Id);
+        builder.Property(b => b.Id).HasColumnName("Id").IsRequired();
+        builder.Property(b => b.Name).HasColumnName("Name").IsRequired();
+        builder.Property(b => b.CreatedAt).HasColumnName("CreatedAt").IsRequired();
+        builder.Property(b => b.UpdatedAt).HasColumnName("UpdatedAt");
+        builder.Property(b => b.DeletedAt).HasColumnName("DeletedAt");
+
+        builder.HasQueryFilter(b => !b.DeletedAt.HasValue);
     }
 }
