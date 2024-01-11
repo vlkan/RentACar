bff68392b9a6a5e92ab06273bf3820898d665017
volkan.onder <volkan.onder@arksoft.com.tr>
2024-01-11 18:33:04 +0300

    automapper implementation

--- Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,4 +1,7 @@
-﻿using MediatR;
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
 
 namespace Application.Features.Brands.Commands.Create;
 
@@ -9,13 +12,25 @@ public class CreateBrandCommand : IRequest<CreatedBrandResponse>
     //CQRS icin command ne ise handler de o dur. Birbiri ile calisirlar.
     public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
     {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+
         public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
         {
-            CreatedBrandResponse response = new CreatedBrandResponse();
-            response.Name = request.Name;
-            response.Id = new Guid();
+            Brand brand = _mapper.Map<Brand>(request);
+            brand.Id = Guid.NewGuid();
 
-            return response;
+            var result = await _brandRepository.AddAsync(brand);
+
+            CreatedBrandResponse createdBrandResponse = _mapper.Map<CreatedBrandResponse>(result);
+
+            return createdBrandResponse;
         }
     }
 }
