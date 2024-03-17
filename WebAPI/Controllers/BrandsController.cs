e24f6403b42e648e31fa785bc5f6eb2375e04ce4
vlkan <thkvolkanonder@gmail.com>
2024-03-18 00:00:49 +0300

    GetList eklendi

--- WebAPI/Controllers/BrandsController.cs
+++ WebAPI/Controllers/BrandsController.cs
@@ -1,4 +1,7 @@
 ﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Queries.GetList;
+using Core.Application.Requests;
+using Core.Application.Responses;
 using Microsoft.AspNetCore.Mvc;
 using WebAPI.Controllers;
 
@@ -8,10 +11,19 @@ namespace WebAPI.Kontrollerse;
 [ApiController]
 public class BrandsController : BaseController
 {
-    [HttpPost]
+    [HttpPost("Add")]
     public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand) 
     {
         CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+        return Ok(response);
+    }
+
+    [HttpGet("GetList")]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListBrandQuery getListBrandQuery = new() { PageRequest = pageRequest };
+        GetListResponse<GetListBrandListItemDto> response = await Mediator.Send(getListBrandQuery);
+
         return Ok(response);
     }
 }
