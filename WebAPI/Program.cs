359ed5c668d3748b07a764cd1632e8730b4c7aae
vlkan <thkvolkanonder@gmail.com>
2025-07-29 23:30:36 +0300

    usage global exception

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Core.CrossCuttingConcerns.Exceptions.Extensions;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+if (app.Environment.IsProduction())
+{
+    app.ConfigureCustomExceptionMiddleware();
 }
 
 app.UseHttpsRedirection();
